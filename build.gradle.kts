12b65ca1b779e8c9af6ffeb8b814d871a555f9e5
Rahul Somasunderam <rahul.som@gmail.com>
2023-02-24 20:10:29 -0800

    chore: Add extra deps

--- build.gradle.kts
+++ build.gradle.kts
@@ -34,17 +34,23 @@ repositories {
 
 dependencies {
   implementation(platform("org.openrewrite.recipe:rewrite-recipe-bom:1.15.2"))
+  implementation("org.openrewrite.recipe:rewrite-java-dependencies")
+  implementation("org.openrewrite.recipe:rewrite-java-security")
+  implementation("org.openrewrite.recipe:rewrite-kubernetes")
   implementation("org.openrewrite.recipe:rewrite-logging-frameworks")
   implementation("org.openrewrite.recipe:rewrite-migrate-java")
+  implementation("org.openrewrite.recipe:rewrite-spring")
   implementation("org.openrewrite.recipe:rewrite-testing-frameworks")
   implementation("org.openrewrite:rewrite-gradle")
   implementation("org.openrewrite:rewrite-groovy")
-  implementation("org.openrewrite:rewrite-java-17")
   implementation("org.openrewrite:rewrite-java")
+  implementation("org.openrewrite:rewrite-java-17")
   implementation("org.openrewrite:rewrite-json")
+  implementation("org.openrewrite:rewrite-kotlin")
   implementation("org.openrewrite:rewrite-properties")
   implementation("org.openrewrite:rewrite-xml")
   implementation("org.openrewrite:rewrite-yaml")
+
   implementation("info.picocli:picocli:4.7.1")
 
   testImplementation("org.junit.jupiter:junit-jupiter-api:5.9.2")
